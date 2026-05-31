a227ee62f0ce4b02d4249f3a51663366e1fb3958
sofiazlobina <zlobinasophya@gmail.com>
2026-05-31 11:48:46 +0300

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/main.jsx',
+    '!src/index.css'
+  ]
+};
