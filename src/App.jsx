d0169edacc4121fabc88404d169b64ef71198e6c
sofiazlobina <zlobinasophya@gmail.com>
2026-05-31 11:47:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import List from './pages/List';
-import Details from './pages/Details';
-import About from './pages/About';
-import Favorites from './pages/Favorites';
+import LoadingSpinner from './components/LoadingSpinner';
 import ErrorBoundary from './components/ErrorBoundary';
+
+// Ленивая загрузка страниц
+const Home = lazy(() => import('./pages/Home'));
+const List = lazy(() => import('./pages/List'));
+const Details = lazy(() => import('./pages/Details'));
+const About = lazy(() => import('./pages/About'));
+const Favorites = lazy(() => import('./pages/Favorites'));
 
 function App() {
   return (
@@ -13,13 +17,15 @@ function App() {
       <div className="app">
         <Navbar />
         <main className="app__main">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/list/:id" element={<Details />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Routes>
+          <Suspense fallback={<LoadingSpinner />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/list/:id" element={<Details />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+          </Suspense>
         </main>
         <footer className="app__footer">
           <p>© 2026 React App. Учебный проект.</p>
